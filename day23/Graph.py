c3cef99f581be0da541b569d22fbf43041e0bff3
Joseph Cheng <josephchengyx@gmail.com>
2025-09-12 02:49:52 -0400

    Add day23 part 1

--- day23/Graph.py
+++ day23/Graph.py
@@ -1,163 +1,148 @@
-import math
-import heapq
-import itertools
+from collections import deque
 
-class Node:
-    def __init__(self, name, node_type):
-        self.name = name # str
-        self.node_type = node_type # str
-        self.neighbours = dict() # {node: edge weight}
-        self.item = None # Item
+class CollectionUtils:
+    @staticmethod
+    def ordered_set(lst):
+        ordset = dict()
+        for elem in lst:
+            ordset[elem] = None
+        return ordset
 
-    def __repr__(self):
-        if self.is_empty():
-            return '.'
-        else:
-            return str(self.item)
+    @staticmethod
+    def dict_to_tuple(dic):
+        return tuple(dic.items())
 
-    def __eq__(self, other):
-        return self.name == other.name and self.node_type == other.node_type
-
-    def __hash__(self):
-        return hash((self.name, self.node_type))
-
-    def is_empty(self):
-        return self.item is None
-
-    def correct_item(self):
-        if self.node_type == "room":
-            return self.name[0]
-        return ''
-
-    def has_correct_item(self):
-        return self.correct_item() == self.item.name
-
-    def move_item_to(self, other):
-        if (self.is_empty() or
-           other not in self.neighbours or
-           not other.is_empty()):
-            return 0
-        other.item = self.item
-        self.item = None
-        other.item.move_to(other)
-        return self.neighbours[other] * other.item.cost()
+    @staticmethod
+    def tuple_to_dict(tup):
+        return dict(tup)
 
 
 class Graph:
-    rooms = ["A1", "B1", "C1", "D1", "A2", "B2", "C2", "D2"]
-    halls = ["L2", "L1", "AB", "BC", "CD", "R1", "R2"]
+    rooms = CollectionUtils.ordered_set(["A1", "B1", "C1", "D1", "A2", "B2", "C2", "D2"])
+    halls = CollectionUtils.ordered_set(["L2", "L1", "AB", "BC", "CD", "R1", "R2"])
+    doors = CollectionUtils.ordered_set(["A0", "B0", "C0", "D0"])
 
     def __init__(self):
-        self.nodes = dict() # {Node.name: Node}
-        self.items = dict() # {Item: Node.name}
+        self.items = dict() # {Item: (str) node}
+        self.nodes = dict() # {(str) node: Item}
 
-        # initialize nodes
-        for node in Graph.rooms:
-            self.nodes[node] = Node(node, "room")
-
-        for node in Graph.halls:
-            self.nodes[node] = Node(node, "hall")
-
-        # link up nodes
-        self.nodes["A1"].neighbours = {self.nodes["A2"]:1,
-                                            self.nodes["L1"]:2,
-                                            self.nodes["AB"]:2}
-        self.nodes["A2"].neighbours = {self.nodes["A1"]:1}
-        self.nodes["B1"].neighbours = {self.nodes["B2"]:1,
-                                            self.nodes["AB"]:2,
-                                            self.nodes["BC"]:2}
-        self.nodes["B2"].neighbours = {self.nodes["B1"]:1}
-        self.nodes["C1"].neighbours = {self.nodes["C2"]:1,
-                                            self.nodes["BC"]:2,
-                                            self.nodes["CD"]:2}
-        self.nodes["C2"].neighbours = {self.nodes["C1"]:1}
-        self.nodes["D1"].neighbours = {self.nodes["D2"]:1,
-                                            self.nodes["CD"]:2,
-                                            self.nodes["R1"]:2}
-        self.nodes["D2"].neighbours = {self.nodes["D1"]:1}
-        self.nodes["L1"].neighbours = {self.nodes["L2"]:1,
-                                            self.nodes["A1"]:2,
-                                            self.nodes["AB"]:2}
-        self.nodes["L2"].neighbours = {self.nodes["L1"]:1}
-        self.nodes["R1"].neighbours = {self.nodes["R2"]:1,
-                                            self.nodes["D1"]:2,
-                                            self.nodes["CD"]:2,}
-        self.nodes["R2"].neighbours = {self.nodes["R1"]:1}
-        self.nodes["AB"].neighbours = {self.nodes["L1"]:2,
-                                            self.nodes["A1"]:2,
-                                            self.nodes["B1"]:2,
-                                            self.nodes["BC"]:2}
-        self.nodes["BC"].neighbours = {self.nodes["AB"]:2,
-                                            self.nodes["B1"]:2,
-                                            self.nodes["C1"]:2,
-                                            self.nodes["CD"]:2}
-        self.nodes["CD"].neighbours = {self.nodes["BC"]:2,
-                                            self.nodes["C1"]:2,
-                                            self.nodes["D1"]:2,
-                                            self.nodes["R1"]:2}
+        self.adjacency_list = {
+            "A1": ["A2", "A0"],
+            "B1": ["B2", "B0"],
+            "C1": ["C2", "C0"],
+            "D1": ["D2", "D0"],
+            "A2": ["A1"],
+            "B2": ["B1"],
+            "C2": ["C1"],
+            "D2": ["D1"],
+            "L2": ["L1"],
+            "L1": ["L2", "A0"],
+            "A0": ["A1", "L1", "AB"],
+            "AB": ["A0", "B0"],
+            "B0": ["B1", "AB", "BC"],
+            "BC": ["B0", "C0"],
+            "C0": ["C1", "BC", "CD"],
+            "CD": ["C0", "D0"],
+            "D0": ["D1", "CD", "R1"],
+            "R1": ["D0", "R2"],
+            "R2": ["R1"],
+        }
 
     def __repr__(self):
+        n = {node: str(item) if not item is None else '.' for node, item in self.nodes.items()}
         layout = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-                  ['#', self.nodes["L2"], self.nodes["L1"], '.', self.nodes["AB"], '.', self.nodes["BC"],
-                   '.', self.nodes["CD"], '.', self.nodes["R1"], self.nodes["R2"], '#'],
-                  ['#', '#', '#', self.nodes["A1"], '#', self.nodes["B1"], '#',
-                   self.nodes["C1"], '#', self.nodes["D1"], '#', '#', '#'],
-                  [' ', ' ', '#', self.nodes["A2"], '#', self.nodes["B2"], '#',
-                   self.nodes["C2"], '#', self.nodes["D2"], '#', ' ', ' '],
+                  ['#', n["L2"], n["L1"], n["A0"], n["AB"], n["B0"], n["BC"],
+                   n["C0"], n["CD"], n["D0"], n["R1"], n["R2"], '#'],
+                  ['#', '#', '#', n["A1"], '#', n["B1"], '#', n["C1"], '#', n["D1"], '#', '#', '#'],
+                  [' ', ' ', '#', n["A2"], '#', n["B2"], '#', n["C2"], '#', n["D2"], '#', ' ', ' '],
                   [' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ']]
         return '\n'.join(map(lambda row: ''.join(map(str, row)), layout))
 
-    def __hash__(self):
-        return hash(self.state())
-
     @staticmethod
-    def initialize_graph(starting_config):
+    def initialize_from(starting_config):
         graph = Graph()
-        counter = itertools.count()
+        item_counter = dict()
         for node, item in zip(Graph.rooms, starting_config):
-            item = Item(item, next(counter))
-            item.node = graph.nodes[node]
-            graph.nodes[node].item = item
+            if item not in item_counter:
+                item_counter[item] = 0
+            else:
+                item_counter[item] += 1
+            item = Item(item, item_counter[item])
             graph.items[item] = node
+        graph.nodes = Graph.items_by_nodes(graph.items)
         return graph
 
     @staticmethod
-    def from_item_locations(items):
-        graph = Graph()
-        for item, node in items.items():
-            item = item.copy()
-            item.node = graph.nodes[node]
-            graph.nodes[node].item = item
-            graph.items[item] = node
-        return graph
+    def room_type(node):
+        if node in Graph.rooms:
+            return "room"
+        elif node in Graph.halls:
+            return "hall"
+        elif node in Graph.doors:
+            return "door"
+        else:
+            return ""
 
     @staticmethod
     def get_rooms_for(item):
-        return [f"{item.name}1", f"{item.name}2"]
+        return list(filter(lambda node: node[0] == str(item), Graph.rooms))
 
-    def copy(self):
-        return Graph.from_item_locations(self.items)
+    @staticmethod
+    def items_by_nodes(items):
+        nodes = {node: None for node in Graph.rooms | Graph.halls | Graph.doors}
+        for item, node in items.items():
+            nodes[node] = item
+        return nodes
 
     def state(self):
-        return tuple([(node, item.name) for item, node in self.items.items()])
+        return CollectionUtils.dict_to_tuple(
+            {item: node for item, node in self.items.items()})
+
+    def set_state(self, items):
+        items = CollectionUtils.tuple_to_dict(items)
+        if self.items != items:
+            self.items = items
+            self.nodes = Graph.items_by_nodes(items)
+
+    def node_of(self, item):
+        return self.items[item]
+
+    def item_at(self, node):
+        return self.nodes[node]
+
+    def update_position(self, item, new_node):
+        orig_node = self.node_of(item)
+        self.items[item] = new_node
+        self.nodes[new_node] = item
+        self.nodes[orig_node] = None
+
+    def neighbours(self, node):
+        return self.adjacency_list[node]
+
+    def is_empty(self, node):
+        return self.item_at(node) is None
+
+    def has_correct_item(self, node):
+        if not self.is_empty(node):
+            return self.item_at(node).in_correct_room(node)
+        return False
 
     def rooms_are_available(self, item):
         for node in Graph.get_rooms_for(item):
-            if not (self.nodes[node].is_empty() or
-                    self.nodes[node].has_correct_item()):
+            if not (self.is_empty(node) or self.has_correct_item(node)):
                 return False
         return True
 
     def get_available_rooms(self, item):
         rooms = list()
         for node in Graph.get_rooms_for(item):
-            if self.nodes[node].is_empty():
+            if self.is_empty(node):
                 rooms.append(node)
         return rooms[::-1]
 
-    def has_correct_config(self):
-        for item in self.items:
-            if not item.in_correct_room():
+    def is_done(self):
+        for item, node in self.items.items():
+            if not self.is_empty(node) and not item.in_correct_room(node):
                 return False
         return True
 
@@ -165,27 +150,20 @@ class Graph:
         def backtrack(node):
             path = list()
             while not node is None:
-                path.append(node.name)
+                path.append(node)
                 node = visited[node]
             return path[::-1]
 
-        src_node, dst_node = self.nodes[src_node], self.nodes[dst_node]
-        counter = itertools.count() # only for breaking ties between items in the pqueue
-        pqueue = [(0, next(counter), src_node, None)]
-        distances = {node: math.inf for node in self.nodes.values()}
+        queue = deque([(src_node, None)])
         visited = dict()
-
-        while pqueue:
-            curr_dist, _, curr_node, prev_node = heapq.heappop(pqueue)
+        while queue:
+            curr_node, prev_node = queue.popleft()
             visited[curr_node] = prev_node
             if curr_node == dst_node:
                 break
-            for next_node, next_dist in curr_node.neighbours.items():
-                if (next_node not in visited and next_node.is_empty()
-                    and curr_dist + next_dist < distances[next_node]):
-                    distances[next_node] = curr_dist + next_dist
-                    heapq.heappush(pqueue,
-                        (curr_dist + next_dist, next(counter), next_node, curr_node))
+            for next_node in self.neighbours(curr_node):
+                if next_node not in visited and self.is_empty(next_node):
+                    queue.append((next_node, curr_node))
 
         if dst_node in visited:
             return True, backtrack(dst_node)
@@ -195,29 +173,27 @@ class Graph:
     def get_possible_moves(self):
         moves = list()
         for item in self.items:
-            if item.in_correct_room(): # don't need to move item
+            curr_node = self.node_of(item)
+            if item.in_correct_room(curr_node) and self.rooms_are_available(item): # don't need to move item
                 continue
-            curr_node = self.items[item]
-            if item.frozen and self.rooms_are_available(item): # move item to correct room
+            if self.rooms_are_available(item): # move item to correct room
                 next_node = self.get_available_rooms(item)[0]
-                if self.has_path(curr_node, next_node):
+                has_path, _ = self.has_path(curr_node, next_node)
+                if has_path:
                     moves.append((curr_node, next_node))
-            else: # move to hallway
+            elif Graph.room_type(curr_node) == "room": # move to hallway
                 for next_node in Graph.halls:
-                    if self.has_path(curr_node, next_node):
+                    has_path, _ = self.has_path(curr_node, next_node)
+                    if has_path:
                         moves.append((curr_node, next_node))
         return moves
 
     def move_item(self, src_node, dst_node):
+        item = self.item_at(src_node)
         has_path, path = self.has_path(src_node, dst_node)
-        if has_path:
-            total_cost = 0
-            for curr_node, next_node in itertools.pairwise(path):
-                curr_node, next_node = self.nodes[curr_node], self.nodes[next_node]
-                total_cost += curr_node.move_item_to(next_node)
-            moved_item = self.nodes[dst_node].item
-            self.items[moved_item] = dst_node
-            return total_cost
+        if item is not None and has_path:
+            self.update_position(item, dst_node)
+            return (len(path) - 1) * item.cost()
         else:
             return 0
 
@@ -225,31 +201,15 @@ class Graph:
 class Item:
     values = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
 
-    def __init__(self, name, internal_id):
+    def __init__(self, name, _id):
         self.name = name # str
-        self.node = None # Node
-        self.frozen = False
-        self._internal_id = internal_id # int
+        self._id = _id # int
 
     def __repr__(self):
         return self.name
 
-    def __eq__(self, other):
-        return self.name == other.name and self._internal_id == other._internal_id
-
-    def __hash__(self):
-        return hash((self.name, self._internal_id))
-
-    def copy(self):
-        return Item(self.name, self._internal_id)
-
-    def in_correct_room(self):
-        return self.node.has_correct_item()
-
-    def move_to(self, node):
-        self.node = node
-        if not self.frozen:
-            self.frozen = True
+    def in_correct_room(self, node):
+        return Graph.room_type(node) == "room" and self.name == node[0]
 
     def cost(self):
         return Item.values[self.name]
--- day23/day23.py
+++ day23/day23.py
@@ -9,44 +9,53 @@ with open('day23_input.txt', newline='') as f:
     starting_config = re.findall(r"[A-D]", reader)
 
 def uniform_cost_search(graph):
-    def backtrack(graph):
+    def backtrack(state):
         steps = list()
-        while not graph is None:
-            steps.append(graph)
-            graph = visited[graph]
+        while not state is None:
+            steps.append(state)
+            state = visited[state]
         return steps[::-1]
 
-    graph = graph.copy()
     counter = itertools.count()  # only for breaking ties between items in the pqueue
-    pqueue = [(0, next(counter), graph, None)]
+    pqueue = [(0, next(counter), graph.state(), None)]
     visited = dict()
     costs = dict()
     goal = None
 
     while pqueue:
-        curr_cost, _, curr_graph, prev_graph = heapq.heappop(pqueue)
-        visited[curr_graph] = prev_graph
-        if curr_graph.has_correct_config():
-            goal = curr_graph
+        curr_cost, _, curr_state, prev_state = heapq.heappop(pqueue)
+        visited[curr_state] = prev_state
+        graph.set_state(curr_state)
+        if graph.is_done():
+            goal = curr_state
             break
-        for move in curr_graph.get_possible_moves():
-            next_graph = curr_graph.copy()
-            next_cost = next_graph.move_item(*move)
-            if next_graph not in costs:
-                costs[next_graph] = math.inf
-            if (next_graph not in visited and
-                curr_cost + next_cost < costs[next_graph]):
-                costs[next_graph] = curr_cost + next_cost
+
+        moves = graph.get_possible_moves()
+        for move in moves:
+            next_cost = graph.move_item(*move)
+            next_state = graph.state()
+            if next_state not in costs:
+                costs[next_state] = math.inf
+            if (next_state not in visited and
+                curr_cost + next_cost < costs[next_state]):
+                costs[next_state] = curr_cost + next_cost
                 heapq.heappush(pqueue,
-                    (curr_cost + next_cost, next(counter), next_graph, curr_graph))
+                    (curr_cost + next_cost, next(counter), next_state, curr_state))
+            graph.set_state(curr_state)
 
     if not goal is None:
         return costs[goal], backtrack(goal)
     return 0, list()
 
-def part1(starting_config):
-    graph = Graph.initialize_graph(starting_config)
+def organize_items(starting_config):
+    def visualize_steps(graph, steps):
+        for step in steps:
+            graph.set_state(step)
+            print(graph,'\n')
+
+    graph = Graph.initialize_from(starting_config)
     cost, steps = uniform_cost_search(graph)
+    # visualize_steps(graph, steps)
     return cost
 
-print(f"Part 1: {part1(starting_config)}")
+print(f"Part 1: {organize_items(starting_config)}")
